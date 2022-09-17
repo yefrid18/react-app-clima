601e569dc5de6f1f16f7bb06d0cbbd9794cdb413
yefrid18 <yefrid18@gmail.com>
2022-09-17 14:29:09 -0500

    Declarar valores y su tipo

--- src/components/WeatherContainer.tsx
+++ src/components/WeatherContainer.tsx
@@ -1,12 +1,54 @@
+import { useEffect, useState } from "react";
 
-export const WeatherContainer = () => {
+export const WeatherContainer = ({
+  fetchedData,
+  error,
+}: {
+  fetchedData: any;
+  error: string;
+}) => {
+
+  const [weather, setWeather] = useState({
+    city:"",
+    country:"",
+    temperature:0,
+    description:"",
+    icon:"",
+    humidity:"",
+    feels:"",
+    visibility:"",
+    pressure:"",
+    longitude:"",
+    latitude:"",
+    windSpeed:""
+  });
+
+  useEffect(() => {
+    if (fetchedData)
+        setWeather({
+            city: fetchedData.name,
+            country: fetchedData.sys.country,
+            temperature: Math.floor(fetchedData.main.temp -273),
+            description: fetchedData.weather[0].description,
+            icon: `http://openweathermap.org/img/wn/${fetchedData.weather[0].icon}@2x.png`,
+            humidity: fetchedData.main.humidity + "%",
+            feels: Math.floor(fetchedData.main.feels_like - 273) + "°C",
+            visibility: fetchedData.visibility + "m",
+            pressure: fetchedData.main.pressure + "hPa",
+            longitude: fetchedData.coord.lon,
+            latitude: fetchedData.coord.lat,
+            windSpeed: fetchedData.wind.speed + "m/s",
+        });
+  }, [fetchedData])
+  
+  
   return (
     <main className="w-96 rounded-3xl bg-gradient-to-b from-gray-800 to-gray-700 shadow-lg">
-        <div className="flex w-full items-center flex-col p-8">
-            <h1>Localización</h1>
-            <h1>Grados</h1>
-            <h1>Tabla de Información</h1>
-        </div>
+      <div className="flex w-full items-center flex-col p-8">
+        <h1>Localización</h1>
+        <h1>Grados</h1>
+        <h1>Tabla de Información</h1>
+      </div>
     </main>
-  )
-}
+  );
+};
