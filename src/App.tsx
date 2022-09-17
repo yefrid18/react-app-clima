952a5b381d365769d2cc010f75d64819192aaf26
yefrid18 <yefrid18@gmail.com>
2022-09-17 15:56:24 -0500

    consumo de datos desde Api con geolocalización

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { WeatherContainer } from "./components/WeatherContainer"
+import { useEffect, useState } from "react";
+import { getWeatherByCoords } from "./api/fetchWeather";
+import { WeatherContainer } from "./components/WeatherContainer";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const LAT = position.coords.latitude;
+      const LON = position.coords.longitude;
+
+      try {
+        const data = await getWeatherByCoords(LAT, LON);
+        setFetchedData(data);
+
+
+      } catch (err) {
+        setError("Error de Localización - Revise su conexión a internet");
+      }
+    });
+  }, []);
 
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
-     <WeatherContainer />
+      <WeatherContainer fetchedData={fetchedData} error={error} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
